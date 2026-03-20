204e4cf0cd742ed475d1a98f3589e1b98c789e92
Abigael Nyakara <abbynyakara@gmail.com>
2026-03-20 09:10:47 -0400

    Completed code

--- 8. Heaps/main.py
+++ 8. Heaps/main.py
@@ -19,6 +19,9 @@ class MaxHeap:
     def _swap(self, index1, index2):
         self.heap[index1], self.heap[index2] = self.heap[index2], self.heap[index1]
 
+    def _sink_down(self, index):
+        pass
+
     def insert(self, value):
         self.heap.append(value)
         current = len(self.heap) - 1
@@ -26,6 +29,30 @@ class MaxHeap:
         while current > 0 and self.heap[current] > self.heap[self._parent(current)]:
             self._swap(current, self._parent(current))
             current = self._parent(current)
+
+    def remove(self):
+        # Removes the node from the Heap
+        # TODO 1 - When its empty
+        if self.heap == 0:
+            return None
+
+        # TODO 2 - When there is only one value in the node.
+        if self.heap == 1:
+            return self.heap.pop()
+
+        # TODO 3 - When there is 2 or more
+
+        max_value = self.heap.pop(0)
+
+        self.heap[0] = self.heap.pop()
+
+        self._sink_down(0)
+
+        return max_value
+
+
+
+
 
             
 my_heap = MaxHeap()
