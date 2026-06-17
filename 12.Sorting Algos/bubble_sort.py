ba34d8ade64dde71bc4ca13d084865f0ff8af99c
Abigael Nyakara <abbynyakara@gmail.com>
2026-06-17 14:33:22 -0400

    Add revision code

--- 12.Sorting Algos/bubble_sort.py
+++ 12.Sorting Algos/bubble_sort.py
@@ -1,13 +1,13 @@
 def bubble_sort(my_list):
-    for i in range(len(my_list)-1, 0, -1): # i = 5 in round 1 if the list has 6 items
+    for i in range(len(my_list) - 1, 0, -1):
         for j in range(i):
             if my_list[j] > my_list[j+1]:
                 temp = my_list[j]
                 my_list[j] = my_list[j+1]
                 my_list[j+1] = temp
+
+
     return my_list
 
 
-print(bubble_sort([2,5,7,4,3]))
-
-    
+print(bubble_sort([12, 7, 4, 21]))
